fba73dd1010e97655a6cbc9ce439b92d4ce9aeca
Roger Johansson <roger@asynkron.se>
2025-09-29 18:10:54 +0200

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,23 @@
 import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Explore the docs
-          </Link>
-        </div>
-      </div>
-    </header>
+    <section className={styles.heroVideoSection}>
+      <video
+        className={styles.heroVideo}
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source src="/assets/img/asynkron/retro.mp4" type="video/mp4" />
+      </video>
+    </section>
   );
 }
 
